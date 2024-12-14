cc4dd1e270b91f6a4d3e57c9b53f478eb51a5646
Ishwar <ishwartrada15@gmail.com>
2024-12-14 19:24:47 +0530

    Initialize Admin Panel work of React app with Vite, Tailwind CSS, and context providers

---
+++ admin/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import Login from "./pages/Login.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AdminContext } from "./context/AdminContext.jsx";
+
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+  return aToken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
