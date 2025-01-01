c4de5516410cc7ae48b4be86a1e27d0c33971696
Ishwar <ishwartrada15@gmail.com>
2025-01-01 16:07:31 +0530

    Implement role checking and logout functionality in basic controller

--- backend/routes/basic.routes.js
+++ backend/routes/basic.routes.js
@@ -1,24 +1,10 @@
 import express, { Router } from "express";
-
-import verifyTokenAndRole from "../middlewares/authAdmin.middleware.js";
+import { checkRole, logout } from "../controllers/basic.controller.js";
 
 const basicRouter = Router();
 
-basicRouter.get("/check-role", (req, res) => {
-  const token = req.cookies.token; // Extract the token from the cookie
+basicRouter.get("/check-role", checkRole);
 
-  if (!token) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid or expired token" });
-    }
-
-    // Return the role of the user to the frontend
-    res.json({ role: decoded.role });
-  });
-});
+basicRouter.post("/logout", logout);
 
 export default basicRouter;
