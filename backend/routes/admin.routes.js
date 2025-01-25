99f0fffac5983b34a9f03be600bca2ba614a7f3d
Ishwar <ishwartrada15@gmail.com>
2025-01-25 19:36:50 +0530

    Refactor admin routes to use JWT verification and role checking middleware

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,11 +1,16 @@
 import express, { Router } from "express";
 import { addDoctor, loginAdmin } from "../controllers/admin.controller.js";
 import upload from "../middlewares/multer.middleware.js";
-import verifyTokenAndRole from "../middlewares/authAdmin.middleware.js";
+import { verifyJwt, isAdmin } from "../middlewares/auth.middleware.js";
 
 const adminRouter = Router();
 
-adminRouter.post("/add-doctor", verifyTokenAndRole("admin"), upload.single("image"), addDoctor);
-adminRouter.post("/login", loginAdmin);
+adminRouter.post(
+  "/add-doctor",
+  verifyJwt,
+  isAdmin,
+  upload.single("image"),
+  addDoctor
+);
 
 export default adminRouter;
