59a54b08bb9f1e15f089638dd40ecba71403a5da
Ishwar <ishwartrada15@gmail.com>
2025-02-01 15:33:28 +0530

    Add js-cookie and jwt-decode for token management and role extraction in AppContext

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,14 +1,29 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets_frontend/assets.js";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
+  const [role, setRole] = useState("");
+
+  useEffect(() => {
+    const token = Cookies.get("aToken");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      setRole(decodedToken.role);
+    }
+  }, []);
+
+  // const token = Cookies.get("token");
+  // const role = jwtDecode(token).role;
 
   const value = {
-    doctors,
+    role,
     currencySymbol,
+    doctors,
   };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
